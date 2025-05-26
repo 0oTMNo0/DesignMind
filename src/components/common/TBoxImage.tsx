71d0c88ef3e08ece7b4fe1110f282b0ef460152d
taban mehr nabizadeh <taban.nbz@gmail.com>
2025-05-26 23:29:09 +0100

    add library

--- src/components/common/TBoxImage.tsx
+++ src/components/common/TBoxImage.tsx
@@ -2,22 +2,66 @@
 import { IconAdd } from '@/assets/icons';
 import { ThemeContext, ThemeContextType } from '@/contexts/ThemeContext';
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+
+type TBoxImageProps = {
+  onImageSelected?: (uri: string) => void;
+};
 
 // create a component
-const TBoxImage = () => {
+const TBoxImage = (props: TBoxImageProps) => {
   const { selectedTheme } = React.useContext(ThemeContext) as ThemeContextType;
+  const [imageUri, setImageUri] = React.useState<string | null>(null);
+
+  const pickImage = async () => {
+    // Ask for permission
+    // const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    // if (status !== 'granted') {
+    //   alert('Permission to access media library is required!');
+    //   return;
+    // }
+    // // Launch the image picker
+    console.log('1111');
+    // const [status, requestPermission] =
+    //   ImagePicker.useMediaLibraryPermissions();
+    console.log('2222');
+
+    // let result = await ImagePicker.launchImageLibraryAsync({
+    //   mediaTypes: ['images'],
+    //   allowsEditing: true,
+    //   quality: 1,
+    //   // base64: false,
+    // });
+    // console.log('ImagePicker result:', result);
+    // if (!result.canceled && result.assets && result.assets[0].uri) {
+    //   const uri = result.assets[0].uri;
+    //   setImageUri(uri);
+    //   props.onImageSelected && props.onImageSelected(uri);
+    // }
+  };
+
   return (
-    <View
-      style={[
-        styles.box,
-        {
-          borderColor: selectedTheme.colors.text2,
-        },
-      ]}
-    >
-      <IconAdd size={35} color="text2" />
-    </View>
+    <TouchableOpacity activeOpacity={0.7} onPress={pickImage}>
+      <View
+        style={[
+          styles.box,
+          {
+            borderColor: selectedTheme.colors.text2,
+          },
+        ]}
+      >
+        {imageUri ? (
+          <Image
+            source={{ uri: imageUri }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        ) : (
+          <IconAdd size={35} color="text2" />
+        )}
+      </View>
+    </TouchableOpacity>
   );
 };
 
@@ -31,6 +75,11 @@ const styles = StyleSheet.create({
     borderStyle: 'dashed',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 15,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
     borderRadius: 15,
   },
 });
